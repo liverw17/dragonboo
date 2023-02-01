5d64621ebf9ac27c3efa5a7a327105511f33d865
liverw17 <124175325+liverw17@users.noreply.github.com>
2023-02-01 08:50:40 -0700

    Create dragon.py

---
+++ dragon.py
@@ -0,0 +1,78 @@
+p = 0
+
+import turtle 
+t = turtle.Turtle() 
+s = turtle.Screen()
+
+turtle.color('Green')
+
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(180)
+t.forward(100) 
+t.left(90)
+t.forward(400) 
+t.left(90)
+t.forward(100)
+t.left(180)
+t.forward(100)
+t.right(90)
+t.forward(300)
+t.left(90)
+t.forward(200)
+t.right(90)
+t.forward(400)
+t.left(90)
+t.forward(180)
+t.left(90)
+t.forward(600) 
+t.left(90)
+t.forward(300) 
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(40)
+t.left(90)
+t.forward(40)
+t.right(90)
+t.forward(700)
+t.left(90)
+t.forward(20)
+t.left(90)
+t.forward(60)
+t.right(90)
+t.forward(160)
+t.left(90)
+
+
+
+while p<4:
+    p = p+1
+    
+    t.forward(125)
+    t.right(90)
+    t.forward(50)
+    t.right(180)
+    t.forward(50)
+    t.right(90)
+
+
+ 
+    
+
+s.exitonclick()
